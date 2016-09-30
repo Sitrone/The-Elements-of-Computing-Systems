11357bbef214b98b998ccdf8f0fca6ef66d39215
sitrone <sununiq@163.com>
2016-09-30 23:01:42 +0800

    finish assembler

---
+++ projects/06/Assember/Assembler.java
@@ -0,0 +1,154 @@
+﻿
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Assembler {
+	
+	private Parser parser;
+	private SymboleTable sTable;
+	private BufferedReader br = null;
+	private BufferedWriter bw = null;
+	
+	public Assembler(){
+		parser = new Parser();
+		sTable = new SymboleTable();
+	}
+	
+	public void exec(String inFile, String outFile){
+		process1(inFile);
+		
+		process2(inFile, outFile);
+	}
+	
+	private void process1(String inFile){
+		int i = 0;
+		try {
+			br = new BufferedReader(new FileReader(inFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String line = null;
+		try {
+			while((line = br.readLine()) != null){
+				// 跳过空行和注释行
+				if(line.equals("") || line.trim().startsWith("//")){
+					continue;
+				}
+				
+				if(parser.commandType(line) == CmdConst.L_COMMAND){
+					String symbol = parser.symbol(line);
+					if(!(sTable.contains(symbol))){
+						sTable.addEntry(symbol, i);
+					}
+				}else{
+					i++;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try{
+				if(null != br){
+					br.close();
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void process2(String inFile, String outFile){
+		int add = sTable.getNextAddress();
+		
+		try {
+			br = new BufferedReader(new FileReader(inFile));
+			bw = new BufferedWriter(new FileWriter(outFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String line = null;
+		try {
+			while((line = br.readLine()) != null){
+				// 跳过空行和注释行
+				if(line.equals("") || line.trim().startsWith("//")){
+					continue;
+				}
+				
+				String mCode = "";
+				String cType = parser.commandType(line);
+				switch(cType){
+				case CmdConst.A_COMMAND:
+					String aCmd = parser.symbol(line);
+					if(!isNum(aCmd)){
+						if(!(sTable.contains(aCmd))){
+							sTable.addEntry(aCmd, add++);
+						}else{
+							mCode += zfill(Integer.toBinaryString(sTable.getAddress(aCmd)));
+						}
+					}else{
+						mCode += zfill(Integer.toBinaryString(Integer.parseInt(aCmd)));
+					}
+					bw.write("0" + mCode + "\n");
+					break;
+					
+				case CmdConst.C_COMMAND:
+					String dest = Code.dest(parser.dest(line));
+					String comp = Code.comp(parser.comp(line));
+					String jump = Code.jump(parser.jump(line));
+					bw.write("111" + comp + dest + jump + "\n");
+					break;
+				}
+			
+				bw.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try{
+				if(null != br){
+					br.close();
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+			
+			try{
+				if(null != bw){
+					bw.close();
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	private boolean isNum(String target){
+		try {
+			Integer.parseInt(target);
+		}
+		catch (NumberFormatException ex) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	private String zfill(String in){
+		if(in.length() == 15){
+			return in;
+		}else{
+			return zfill("0" + in);
+		}
+	}
+}
---
+++ projects/06/Assember/Code.java
@@ -0,0 +1,113 @@
+﻿import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Code {
+	private Code(){}
+	
+	private static final Map<String, String> myDest = destMap();
+	
+	private static Map<String, String> destMap(){
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("null", "000");
+		result.put("M",    "001");
+		result.put("D",    "010");
+		result.put("MD",   "011");
+		result.put("A",    "100");
+		result.put("AM",   "101");
+		result.put("AD",   "110");
+		result.put("ADM",  "111");
+		return Collections.unmodifiableMap(result);
+	}
+	
+	
+	/**
+	 * 获取对应的dest代码
+	 * @param target
+	 * @return
+	 */
+	public static String dest(String target){
+		String resultBits = null;
+
+		if(myDest.containsKey(target)){
+			resultBits = myDest.get(target);
+		}
+		return resultBits;
+	}
+	
+	// comp 获取
+	private static final Map<String, String> myComp = compMap();
+	
+	private static Map<String, String> compMap(){
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("0",   "0101010");
+		result.put("1",   "0111111");
+		result.put("-1",  "0111010");
+		result.put("D",   "0001100");
+		result.put("A",   "0110000");
+		result.put("!D",  "0001101");
+		result.put("!A",  "0110001");
+		result.put("-D",  "0001111");
+		result.put("-A",  "0110011");
+		result.put("D+1", "0011111");
+		result.put("A+1", "0110111");
+		result.put("D-1", "0001110");
+		result.put("A-1", "0110010");
+		result.put("D+A", "0000010");
+		result.put("D-A", "0010011");
+		result.put("A-D", "0000111");
+		result.put("D&A", "0000000");
+		result.put("D|A", "0010101");
+		
+		result.put("M",   "1110000");
+		result.put("!M",  "1110001");
+		result.put("-M",  "1110011");
+		result.put("M+1", "1110111");
+		result.put("M-1", "1110010");
+		result.put("D+M", "1000010");
+		result.put("D-M", "1010011");
+		result.put("M-D", "1000111");
+		result.put("D&M", "1000000");
+		result.put("D|M", "1010101");
+		return Collections.unmodifiableMap(result);
+	}
+	
+	/**
+	 * 
+	 * @param target
+	 * @return
+	 */
+	public static String comp(String target){
+		String resultBits = null;
+
+		if(myComp.containsKey(target)){
+			resultBits = myComp.get(target);
+		}
+		return resultBits;
+	}
+	
+	// jump获取
+	private static final Map<String, String> myJump = jumpMap();
+	
+	private static Map<String, String> jumpMap(){
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("null",  "000");
+		result.put("JGT",   "001");
+		result.put("JEQ",   "010");
+		result.put("JGE",   "011");
+		result.put("JLT",   "100");
+		result.put("JNE",   "101");
+		result.put("JLE",   "110");
+		result.put("JMP",  "111");
+		return Collections.unmodifiableMap(result);
+	}
+	
+	public static String jump(String target){
+		String resultBits = null;
+
+		if(myJump.containsKey(target)){
+			resultBits = myJump.get(target);
+		}
+		return resultBits;
+	}
+}
